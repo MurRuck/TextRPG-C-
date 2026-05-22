371e1c55c852db7492171731ebc619da1429baf7
CarryMachine <jungkyusang@gmail.com>
2026-05-22 12:21:53 +0900

    feat: implement gamemode

--- TextRpg/GameManager/Gamemode/Gamemode.h
+++ TextRpg/GameManager/Gamemode/Gamemode.h
@@ -1,7 +1,42 @@
 ﻿#pragma once
+#include <cstdlib>
+#include <memory>
+
+#include "../../Entity/Character/PlayerCharacter.h"
+
+class MapBase;
+
+enum class PlayerState { Create, Battle, Shop };
 
 class Gamemode
 {
 public:
-    
+    Gamemode();
+    ~Gamemode();
+
+
+    // player getter
+    const PlayerCharacter* GetPlayerCharacter() const { return player.get(); }
+
+    // player setter
+    void SetPlayerCharceter(std::unique_ptr<PlayerCharacter> newCharacter)
+    {
+        player = std::move(newCharacter);
+    }
+
+    // state getter
+    PlayerState GetPlayerState() const { return currentState; }
+
+    // state setter
+    void SetPlayerState(PlayerState newState) { currentState = newState; }
+
+    // UI
+    void InitUI();
+    void UIRenderer();
+    void HandleInput();
+    void ClearCLI() { std::system("cls"); }
+
+private:
+    std::unique_ptr<PlayerCharacter> player;
+    PlayerState currentState = PlayerState::Create;
 };
---
+++ TextRpg/Map/MapBase.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#include "../GameManager/Gamemode/Gamemode.h"
+
+class MapBase
+{
+    ~MapBase() = default;
+};
