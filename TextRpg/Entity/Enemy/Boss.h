ec126ee1907424a19a4814bf2aded2813bde85a1
Jm <jm170922@gmail.com>
2026-05-22 12:43:21 +0900

    feat: 몬스터/보스 클래스 구현

---
+++ TextRpg/Entity/Enemy/Boss.h
@@ -0,0 +1,10 @@
+// Boss.h
+
+#pragma once
+#include "EnemyBase.h"
+
+class Boss : public EnemyBase
+{
+public:
+	Boss(std::string name, int level);
+};
--- TextRpg/Entity/Enemy/EnemyBase.cpp
+++ TextRpg/Entity/Enemy/EnemyBase.cpp
@@ -1,1 +1,58 @@
-﻿#include "EnemyBase.h"
+// EnemyBase.cpp
+
+#include "EnemyBase.h"
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+EnemyBase::EnemyBase(string name, int level, float statMultiplier)
+{
+	this->name = name;
+	hp = ((rand() % (level * 10 + 1)) + level * 20) * statMultiplier;
+	attack = ((rand() % (level * 5 + 1)) + level * 5) * statMultiplier;
+	exp = level * 10; // 경험치량은 임시
+	gold = level * 10; // 골드도 임시
+}
+
+string EnemyBase::GetName() const
+{
+	return name;
+}
+
+int EnemyBase::GetHp() const
+{
+	return hp;
+}
+
+int EnemyBase::GetAttack() const
+{
+	return attack;
+}
+
+int EnemyBase::GetExp() const
+{
+	return exp;
+}
+
+int EnemyBase::GetGold() const
+{
+	return gold;
+}
+
+void EnemyBase::TakeDamage(int damage)
+{
+	hp -= damage;
+	if (hp < 0) hp = 0;
+}
+
+bool EnemyBase::IsAlive() const
+{
+	return hp < 0;
+}
+
+void EnemyBase::PrintInfo() const
+{
+	cout << "[ " << name << " ]" << endl;
+	cout << "HP: " << hp << endl;
+	cout << "ATK: " << attack << endl;
+}
