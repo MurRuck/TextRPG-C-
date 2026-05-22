3bc1b4b4aaa18cb859a3a254478ad884ceffba58
CarryMachine <jungkyusang@gmail.com>
2026-05-22 16:17:18 +0900

    feat: 로그 구현 일부

--- TextRpg/GameManager/LoggingManager/LoggingManager.cpp
+++ TextRpg/GameManager/LoggingManager/LoggingManager.cpp
@@ -1,1 +1,67 @@
 ﻿#include "LoggingManager.h"
+#include <filesystem>
+
+LoggingManager::LoggingManager()
+{
+    
+}
+
+LoggingManager::~LoggingManager()
+{
+    if (logFile.is_open())
+    {
+        logFile.close();
+    }
+}
+
+bool LoggingManager::Initialize(const std::string& logDirectory)
+{
+    namespace fs = std::filesystem;
+    
+    if (!fs::exists(logDirectory))
+    {
+        fs::create_directory(logDirectory);
+    }
+    
+    std::string timestamp = GetTimestamp();
+    currentLogFilePath = logDirectory + "/log" + timestamp + ".txt";
+    
+    logFile.open(currentLogFilePath);
+    
+    return logFile.is_open();
+}
+
+template <typename T>
+void LoggingManager::Add(LogHeader headText, T data)
+{
+    if (!logFile.is_open())
+    {
+        return;
+    }
+    
+    std::string headTextToString;
+    if (headText == LogHeader::Error)
+    {
+        headTextToString = "Error";
+    }
+    if (headText == LogHeader::Warning)
+    {
+        headTextToString = "Warning";
+    }
+    if (headText == LogHeader::Info)
+    {
+        headTextToString = "Info";
+    }
+    
+    std::string logDataString = std::to_string(data);
+    std::string lineForLogging = "[" + headTextToString+ "] " + logDataString;
+    
+    logFile << lineForLogging << std::endl;
+    logFile.flush();
+}
+
+
+void LoggingManager::ViewLog() const
+{
+    
+}
--- TextRpg/GameManager/LoggingManager/LoggingManager.h
+++ TextRpg/GameManager/LoggingManager/LoggingManager.h
@@ -1,7 +1,24 @@
 ﻿#pragma once
+#include <fstream>
+#include <string>
+
+enum class LogHeader { Error, Warning, Info };
 
 class LoggingManager
 {
 public:
+    LoggingManager();
+    ~LoggingManager();
     
+    bool Initialize(const std::string& logDirectory = "Logs");
+    
+    template<typename T>
+    void Add(LogHeader headText, T data);
+    void ViewLog() const;
+    
+private:
+    std::string currentLogFilePath;
+    std::ofstream logFile;
+    
+    std::string GetTimestamp() const;
 };
