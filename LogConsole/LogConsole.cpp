44b4410a8063465490556df115ecb34a8a4db8ed
CarryMachine <jungkyusang@gmail.com>
2026-05-27 09:44:31 +0900

    hotfix: add logconsole

---
+++ LogConsole/LogConsole.cpp
@@ -0,0 +1,18 @@
+﻿ // LogConsole.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+
+ int main()
+{
+    std::cout << "========== TextRPG Logger ==========" << std::endl;
+    
+    std::string line;
+    while (std::getline(std::cin, line))
+    {
+        std::cout << line << std::endl;
+    }
+     
+     return 0;
+}
