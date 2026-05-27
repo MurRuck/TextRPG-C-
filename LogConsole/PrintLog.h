5c5e7e2ee50e5a3528627c3e903a5feaf58f4ea2
CarryMachine <jungkyusang@gmail.com>
2026-05-27 10:00:05 +0900

    hofix: edit loggerconsole

---
+++ LogConsole/PrintLog.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+class PrintLog
+{
+public:
+    void SetColor(WORD color);
+    void PrintLogLine(const std::string& line);
+};
