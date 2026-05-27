16fca63e89da6c190ea9b6d8a770799ee83516bf
CarryMachine <jungkyusang@gmail.com>
2026-05-27 20:48:33 +0900

    hotfix: 주석추가

--- LogConsole/LogConsole.cpp
+++ LogConsole/LogConsole.cpp
@@ -8,6 +8,8 @@
 
 void  SetColor(WORD color)
 {
+    // 터미널 글자색 변경 이게 진짜 터꾸지
+    // GetStdHandle = 현재 출력되는 터미널
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
@@ -15,13 +17,21 @@ void PrintLogLine(const std::string& line)
 {
     HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
     
+    // 현재 터미널의 화면 버퍼 정보 struct
+    // info = 버퍼크기, 커서 위치, 현재 글자/배경 색상, 창 영역/크기 가지고있음
     CONSOLE_SCREEN_BUFFER_INFO info{};
     GetConsoleScreenBufferInfo(console, &info);
+    
+    // wAttributes = 색상 
     WORD color = info.wAttributes;
     
     std::string header;
     WORD headerColor = color;
     
+    // find = index 앞부터 찾기
+    // rfind = reverse -> 뒤에서부터 찾기
+    // offset이 0이라 사실상 문자열 시작이 [Error]인지 검사 근데 앞에 다른 문자가 붙을 수 있음
+    // 그래서 뒤에서부터 탐색
     if (line.rfind("[Error]",0) == 0)
     {
         header = "[Error]";
@@ -62,8 +72,10 @@ int main()
         PrintLogLine(line);
     }
 
-    std::cout << "\n========== Logger disconnected ==========" << std::endl;
+    std::cout << "========== Logger disconnected ==========" << std::endl;
     std::cout << "Press any key to close..." << std::endl;
+    
+    // 키보드에서 아무거나 입력받기 전까지 대기
     _getch();
      
      return 0;
